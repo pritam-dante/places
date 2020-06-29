fa883bfa115ce729b1c5750a6f73d89fab4fb89e
pritam-dante <pritam.dante@gmail.com>
2020-06-29 20:27:10 +0530

    initialization

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './providers/great_places.dart';
+import './screens/place_list_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider.value(
+      value: GreatPlaces(),
+      child: MaterialApp(
+        title: 'GreatPlaces',
+        theme: ThemeData(
+          primaryColor: Colors.indigo,
+          accentColor: Colors.amber,
+        ),
+        home: PlaceListScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/place_list_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PlaceListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('YourPlaces'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
