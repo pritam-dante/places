8c5f8ea0fbfe8624e0bc896f4c28e822953a139c
pritam-dante <pritam.dante@gmail.com>
2020-06-29 22:47:17 +0530

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import './providers/great_places.dart';
 import './screens/place_list_screen.dart';
+import './screens/add_place_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,13 +13,15 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider.value(
       value: GreatPlaces(),
       child: MaterialApp(
-        title: 'GreatPlaces',
-        theme: ThemeData(
-          primaryColor: Colors.indigo,
-          accentColor: Colors.amber,
-        ),
-        home: PlaceListScreen(),
-      ),
+          title: 'GreatPlaces',
+          theme: ThemeData(
+            primaryColor: Colors.indigo,
+            accentColor: Colors.amber,
+          ),
+          home: PlaceListScreen(),
+          routes: {
+            AddPlaceScreen.routeName: (ctx) => AddPlaceScreen(),
+          }),
     );
   }
 }
